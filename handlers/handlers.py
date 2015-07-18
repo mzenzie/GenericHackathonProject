60d7c45d416282f1e4d67967487dd3ace0e31db6
Zelin <dzelin@int-dzeli-osx.hq.corp.viasat.com>
2015-07-18 12:13:20 -0700

    formatted css and html files

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -17,7 +17,69 @@ from tornado.ioloop import IOLoop
 from tornado.web import asynchronous, RequestHandler, Application
 from tornado.httpclient import AsyncHTTPClient
 
-class HomeHandler(BaseHandler):
+class BaseHandler(RequestHandler):
+    def get_login_url(self):
+        return u"/login"
+
+    def get_current_user(self):
+        user_json = self.get_secure_cookie("user")
+        if user_json:
+            return tornado.escape.json_decode(user_json)
+        else:
+            return None
+        
+# Purpose: Login page checks for permission                
+class AuthLoginHandler(BaseHandler):
     def get(self):
-        messages = self.application.syncdb.messages.find()
-        self.render("slidy.html", messages=messages, notification=self.get_flash())
+
+        errormessage = ""
+        try:
+            errormesage = self.get_argument("error")
+        except:
+            errormessage = ""
+        self.render ("login.html", errormessage = errormessage)
+        
+    def check_permission(self, password, username):
+        usernames = ["azheng", "dzelin", "mzenzie"]
+        if username in usernames and password == "Welcome1":
+            return True
+        return False
+
+    def post(self):  
+        username = self.get_argument("username", "")
+        password = self.get_argument("password", "")
+        
+        auth = self.check_permission(password, username)
+        if auth:
+            self.set_current_user(username)
+            self.redirect('/home/')
+        else:
+            form = """<form method = "post" class = "container">
+            <p> <form action = "localhost:8888/login/"></form></p>
+            <h3>Incorrect Username or Password. </h3>
+            <p>
+            
+            <form action = "http://localhost:8888/">
+            <input type = "submit" value = "Try again">
+            </form>
+            
+            </p>
+            </form>"""
+            self.write(form)
+        
+
+    def set_current_user(self, user):
+        if user:
+            self.set_secure_cookie("user", tornado.escape.json_encode(user))
+        else:
+            self.clear_cookie("user")
+            
+# Logout page clears cookie        
+class AuthLogoutHandler(BaseHandler):
+    def get(self):
+        self.clear_cookie("user")
+        self.redirect(self.get_argument("next", "/"))
+        
+class HomePageHandler(BaseHandler):
+    def get(self):   
+        self.render("home.html")
--- server.py
+++ server.py
@@ -8,17 +8,17 @@ import webbrowser
 import Settings
 import tornado.escape
 from decorator import protected
+from tornado.web import url
 define("port", default=8888, help="run on the given port", type=int)
 
+from handlers.handlers import * 
+
 class Application(tornado.web.Application):
-    def __init__(self):
+    def __init__(self, **overrides):
         handlers = [
-            (r"/", MainPageHandler),
-            (r"/auth/login/",AuthLoginHandler),
-            (r"/home/", HomePageHandler),
-            (r"/auth/logout/", AuthLogoutHandler),
-	    
-	    url(r'/', HomeHandler, name='home')
+            url(r'/auth/login/',AuthLoginHandler),
+            url(r'/auth/logout/', AuthLogoutHandler),
+	    url(r'/home/', HomePageHandler, name='home')
 	]
 	
         settings = {
@@ -29,80 +29,6 @@ class Application(tornado.web.Application):
             "login_url": "/auth/login/"
         }
         tornado.web.Application.__init__(self, handlers, **settings)
-        
-
-# Purpose: Getting the current user
-class BaseHandler(tornado.web.RequestHandler):
-    def get_current_user(self):
-        return self.get_secure_cookie("user")
-
-# Purpose: Redirects to the home page
-class MainPageHandler(BaseHandler):
-    @protected
-    def get(self):
-        self.redirect('/auth/login/')
-        
-# Purpose: Login page checks for permission                
-class AuthLoginHandler(BaseHandler):
-    def get(self):
-
-        errormessage = ""
-        try:
-            errormesage = self.get_argument("error")
-        except:
-            errormessage = ""
-        self.render ("login.html", errormessage = errormessage)
-        
-    def check_permission(self, password, username):
-        usernames = ["azheng", "dzelin", "mzenzie"]
-        if username in usernames and password == "Welcome1":
-            return True
-        return False
-
-    def post(self):  
-        username = self.get_argument("username", "")
-        password = self.get_argument("password", "")
-        #helper.set_username(username)
-        
-        auth = self.check_permission(password, username)
-        if auth:
-            self.set_current_user(username)
-            self.redirect('/home/')
-            #self.redirect(self.get_argument("next", u"/home/"))
-        else:
-            form = """<form method = "post" class = "container">
-            <p> <form action = "localhost:8888/login/"></form></p>
-            <h3>Incorrect Username or Password. </h3>
-            <p>
-            
-            <form action = "http://localhost:8888/">
-            <input type = "submit" value = "Try again">
-            </form>
-            
-            </p>
-            </form>"""
-            self.write(form)
-        
-
-    def set_current_user(self, user):
-        if user:
-            self.set_secure_cookie("user", tornado.escape.json_encode(user))
-        else:
-            self.clear_cookie("user")
-            
-# Logout page clears cookie        
-class AuthLogoutHandler(BaseHandler):
-    def get(self):
-        self.clear_cookie("user")
-        self.redirect(self.get_argument("next", "/"))
-        
-class HomePageHandler(BaseHandler):
-    @protected
-    def get(self):   
-        self.write("Welcome to the home page")
-	render_sidebar(self)
-
-
                 
 if __name__ == "__main__":
     tornado.options.parse_command_line()
